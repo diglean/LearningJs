5c297db87000b8d12dff859efbe0e84d24f09a18
diglean <diegoleandro2002@gmail.com>
2022-06-15 10:42:12 -0300

    Template string

---
+++ 09-TemplateString.js
@@ -0,0 +1,9 @@
+const nome = "Dig";
+const idade = 2022 - 2002;;
+
+//  const str = "Olá eu sou " + nome + ". \n E minha idade é "+(2022 - 2002);
+
+const strTemplateString = `Olá, eu sou ${nome}.
+ E minha idade é ${idade}.`
+
+console.log(strTemplateString);
